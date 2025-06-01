683f99c40f7800839f14c3067be240dd57934f1b
zanko <zanko.ebrahim@gmail.com>
2025-06-02 01:09:54 +0300

    added kurdish language

--- app/Views/admin/pages/forms/Customers/show.php
+++ app/Views/admin/pages/forms/Customers/show.php
@@ -1,7 +1,7 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1><?= labels('customer_detail', 'Customer Details') ?></h1>
+            <h1><?= labels('customer_details', 'Customer Details') ?></h1>
             <div class="section-header-breadcrumb">
                 <div class="btn-group mr-2 no-shadow">
                     <a class="btn btn-primary text-white" href="<?= base_url('admin/customers'); ?>" class="btn"><i class="fas fa-list"></i> <?= labels('customers', 'Customers') ?></a>
@@ -16,7 +16,7 @@
                 <div class="card-body">
                     <div class="row mt-sm-4">
                         <div class='col-md-12'>
-                            <h2 class="section-title"> <?= labels($from_title, 'Customer Details') ?> </h2>
+                            <h2 class="section-title"> <?= labels('customer_details', 'Customer Details') ?> </h2>
 
                             <form action="<?= base_url('admin/customers/' . $customer['user_id']) ?>" id="form_customer" enctype="multipart/form-data" accept-charset="utf-8" method="POST">
                                 <input type="hidden" name="_method" value="PUT">
@@ -68,42 +68,14 @@
                                         </label>
                                     </div>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Save</button>&nbsp;
-                                <button type="reset" class="btn btn-info">Reset</button>
+                                <button type="submit" class="btn btn-primary"><?= labels('save', 'Save') ?></button>&nbsp;
+                                <button type="reset" class="btn btn-info"><?= labels('reset', 'Reset') ?></button>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <div class="section">
-        <div class="section-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table table-hover" data-show-export="true" data-export-types="['txt','excel','csv']" data-export-options='{"fileName": "customers-list"}' id="customers_table" data-auto-refresh="true" data-show-columns="true" data-show-toggle="true" data-show-refresh="true" data-toggle="table" data-search-highlight="true" data-server-sort="false" data-page-list="[5, 10, 25, 50, 100, 200, All]" data-url="<?= base_url('admin/customers/customers_table'); ?>" data-side-pagination="server" data-pagination="true" data-search="true" data-server-sort="false">
-                                <thead>
-                                    <tr>
-                                        <th data-radio="true"></th>
-                                        <th data-field="id" data-sortable="true"><?= labels('customer_id', 'Customer ID') ?></th>
-                                        <th data-field="name" data-sortable="true"><?= labels('name', 'Name') ?></th>
-                                        <th data-field="email" data-sortable="true"><?= labels('email', 'Email') ?></th>
-                                        <th data-field="mobile" data-sortable="true"><?= labels('mobile_number', 'Mobile') ?></th>
-                                        <th data-field="balance" data-sortable="true"><?= labels('balance', 'Balance') ?></th>
-                                        <th data-field="debit" data-sortable="true"><?= labels('debit', 'Debit') ?></th>
-                                        <th data-field="status" data-sortable="true"><?= labels('status', 'Status') ?></th>
-                                        <th data-field="actions" data-sortable="true"><?= labels('action', 'Actions') ?></th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     </section>
 </div>
 
